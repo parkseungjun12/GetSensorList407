d86f78408e24d064a04b79d8493d167133355b22
박승준 <qtw32@naver.com>
2021-04-07 20:56:56 +0900

    get sensor list using ScrollView

--- app/src/main/java/com/example/getsensorlist407/MainActivity.java
+++ app/src/main/java/com/example/getsensorlist407/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.getsensorlist407;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mTxtSensors;
+    SensorManager sensorMgr;
+    List<Sensor> sensorList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTxtSensors = findViewById(R.id.textSensors);
+        mTxtSensors.setMovementMethod(new ScrollingMovementMethod());
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorList = sensorMgr.getSensorList(Sensor.TYPE_ALL);
+
+        mTxtSensors.append("(# Sensors: " + sensorList.size() + ")\n\n");
+        for(Sensor sensor : sensorList){ //Enhanced for loop
+            mTxtSensors.append("Sensor name: " + sensor.getName() + "\n");
+            mTxtSensors.append("Sensor type: " + sensor.getType() + "\n\n");
+        }
     }
 }
